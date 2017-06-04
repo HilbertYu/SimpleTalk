36372bdab006256343f1a9d8dc07262872085bcc
Hsin-Hsuan Yu <hsinhsuanyu@gmail.com>
2017-06-04 20:11:23 +0800

    + init

---
+++ server.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "hysocket.h"
+void* rec(void* s)
+{
+    HySocketBase * b = (HySocketBase*)s;
+    while(1)
+    {
+        int len = 0;
+        b->recv(&len , 4);
+        char buf[128] = {0};
+        b->recv_all(buf, len);
+        printf("\n>> :%s\n", buf);
+    }
+
+}
+
+int main(int argc, const char * argv[])
+{
+    HySocketServer * s = new HySocketServer;
+    s->setup("0.0.0.0", 20000);
+    HySocketClient * cl = s->accept();
+    pthread_t p;
+    pthread_create(&p, NULL, rec, cl);
+
+    while (1)
+    {
+        char buf[128];
+        printf("Me:");
+        scanf("%[^\n]s", buf);
+        getchar();
+        int len = strlen(buf);
+        int ret = 0;
+        ret = cl->send_all(&len, 4);
+        ret = cl->send_all(buf, len);
+    }
+    return 0;
+}
